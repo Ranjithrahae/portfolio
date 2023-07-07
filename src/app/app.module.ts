8aca367e1042a63358292a45aa61e2f16d22a19c
Ranjithrahae <ranjithrahae@gmail.com>
2023-07-07 14:04:17 +0530

    skillsection finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HeadingComponent } from './heading/heading.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    HeadingComponent,
+    ProjectsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
