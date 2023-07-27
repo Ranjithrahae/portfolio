be3c2dc4628383c6c4be4c4551adb86f154a282a
Ranjith G <ranjithrahae@gmail.com>
2023-07-27 13:45:04 +0530

    project card style updated

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,6 +8,18 @@ import { Component } from '@angular/core';
 export class ProjectsComponent {
   cards = [
     {
+      img: 'assets/mapty.png',
+      tech1: 'assets/html.svg',
+      tech2: 'assets/CSS3_logo.svg',
+      tech3:
+        'https://cdn.sanity.io/images/m45u25nb/production/093ea39748ad4561f3b9c1370c90e844f0389a24-512x512.png',
+
+      Pname: 'Mapty',
+      git: '',
+      link: '',
+      tl:['html', 'Bootstrap', 'Javascript','GeoLocation API','Netlify Hosting']
+    },
+    {
       img: 'assets/cred.png',
       tech1: 'assets/html.svg',
       tech2: 'assets/bootstrap.jpg',
@@ -17,10 +29,7 @@ export class ProjectsComponent {
       Pname: 'Cred-Clone',
       git: 'https://github.com/Ranjithrahae/cred-clone',
       link: 'https://cred-cloneranjith.netlify.app/',
-      tl1: 'Html',
-      tl2: 'Bootstrap',
-      tl3: 'Javascript',
-      tl4: 'Netlify hosting',
+      tl:['html', 'Bootstrap', 'Javascript','Netlify Hosting']
     },
     {
       img: 'assets/pig.png',
@@ -32,37 +41,18 @@ export class ProjectsComponent {
       Pname: 'PIG-GAME',
       git: 'https://github.com/Ranjithrahae/pig-game',
       link: 'https://pig-gamerahae.netlify.app/',
-      tl1: 'HTML',
-      tl2: 'CSS',
-      tl3: 'Javascript',
+      tl:['html', 'CSS', 'Javascript','Netlify Hosting']
     },
     {
       img: 'assets/weatherapp.png',
-      tech1: 'html.svg',
-      tech2: '',
-      tech3:
-        'https://cdn.sanity.io/images/m45u25nb/production/093ea39748ad4561f3b9c1370c90e844f0389a24-512x512.png',
-
+      tech1: 'assets/html.svg',
+      tech2: 'assets/CSS3_logo.svg',
+      tech3:'assets/angular.svg',
       Pname: 'Weatherapp',
-      git: '',
+      git: 'https://github.com/Ranjithrahae/weatherApp',
       link: '',
-      tl1: 'html',
-      tl2: 'Bootstrap',
-      tl3: 'Javascript',
+      tl:['html', 'Bootstrap', 'Javascript','Open-Weather API','Netlify Hosting']
     },
-    {
-      img: '',
-      tech1: 'html.svg',
-      tech2: '',
-      tech3:
-        'https://cdn.sanity.io/images/m45u25nb/production/093ea39748ad4561f3b9c1370c90e844f0389a24-512x512.png',
-
-      Pname: '',
-      git: '',
-      link: '',
-      tl1: 'html',
-      tl2: 'Bootstrap',
-      tl3: 'Javascript',
-    },
+   
   ];
 }
